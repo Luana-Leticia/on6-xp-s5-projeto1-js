3534f9d5b344446d1bc162bc74b66cd072314549
Luana-Leticia <luanaleticiasouza@hotmail.com>
2020-09-06 14:13:49 -0300

    Adiciona funções de validação de id do produto e de quantidade

--- projeto/index.js
+++ projeto/index.js
@@ -8,15 +8,40 @@ const query = require('synchronous-user-input');
 const { produtos } = db;
 produtos.sort((product1, product2) => product1.preco - product2.preco);
 //console.table(produtos);
-/*
+
+
+const getProductById = id => {
+    const product = produtos.find(product => product.id === id);
+    return (typeof product === 'object') ? product : 'Você digitou um id de um produto que não existe. Tente novamente!';
+}
+
+const validateQuantity = quantity => (quantity > 0) ? quantity : 'Você digitou uma quantidade igual a zero ou negativa. Tente novamente!';
+
+
+const shoppingCart = [];
+
 let stillBuying = 's';
 while (stillBuying.toLowerCase() === 's') {
-    const id = query('Digite o id do produto desejado: ');
-    const quantity = query('Digite a quantidade que gostaria de adquirir: ');
+    let id = query('Digite o id do produto desejado: ');
+    let product = getProductById(id);
+    while(typeof product === "string"){
+        console.log(product);
+        id = parseInt(query('Digite o id do produto desejado: '));
+        product = getProductById(id);
+    }
+    let quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
+    quantity = validateQuantity(quantity);
+    while(typeof quantity === 'string'){
+        console.log(quantity);
+        quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
+        quantity = validateQuantity(quantity);
+    }
+    
     stillBuying = query('Deseja continuar comprando? S ou N: ');
 }
-*/
-const shoppingCart = [];
+
+
+
 /* 
 const hasDiscountCoupon = query('Você possui cupom de desconto? S ou N: ');
 const discountCoupon = query('De quanto é o seu cupom? ');
