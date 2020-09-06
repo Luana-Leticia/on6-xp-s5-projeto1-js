2780f2fe4cbf398d91669a430a1e5bf8c7b5e5be
Luana-Leticia <luanaleticiasouza@hotmail.com>
2020-09-06 12:29:29 -0300

    Imprime tabela de produtos e pede informações ao usuário

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ function falar() {
   return 'Pipipi popopo'
 }
 
+// função normal
+
 const falar2 = function () {
   return "oi";
 }
 
-const falar3 = () =>{
-  return "oi"
-}
+// função anônima
 
-const falar4 = () => "oi";
+const falar3 = () =>{ 
+  return "oi"
+} 
+// arrow function e função anônima
+
+const falar4 = () => "oi"; // arrow function e função anônima
+/*
 
 function dobro(num) {
   return num * 2
@@ -28,15 +34,27 @@ function dobro(num) {
 const dobro2 = function (num){
   return num*2;
 }
+*/
 
+const dobro = (num) => num * 2;
+
+
+/*
 function calcularMedia(nota1, nota2, nota3) {
   const soma = (nota1 + nota2 + nota3)
   const media = soma / 3
   return media
 }
 
+*/
+
+const calcularMedia = (nota1, nota2, nota3) => (nota1 + nota2 + nota3) / 3;
+
 // Vamos refatorar essas funções para a sintaxe de Arrow function
 
+// Quando uma função for guardada numa constante, ela pode ser anônima
+// Na arrow function, o nome function é suprimido
+// Em arrows functions com 1 linha no código, as {} são opcionais e o return também
 
 
 
@@ -47,22 +65,38 @@ console.log('Callback')
 
 // CALLBACK
 // Possuímos as funções: somar, subtrair, multiplicar e dividir.
+/*
 function somar(a, b) {
   return a + b
 }
+*/
 
+const somar = (a, b) => a + b;
+/*
 function subtrair(a, b) {
   return a - b
 }
+*/
 
+const subtrair = (a, b) => a - b;
+
+/*
 function multiplicar(a, b) {
   return a * b
 }
+*/
 
+const multiplicar = (a, b) => a * b;
+
+/*
 function dividir(a, b) {
   return a / b
 }
+*/
 
+const dividir = (a, b) => a / b;
+
+/*
 function ordenar(a, b) {
   if (a <= b) {
     return [a, b]
@@ -71,16 +105,28 @@ function ordenar(a, b) {
   }
 }
 
-///// EU
+*/
+
+const ordenar = (a, b) => (a <= b) ? [a, b] : [b, a];
+
+// Callbacks: funções que são passadas como parâmetros de outra(s)
+// Qualquer função pode ser passada como parâmetro
+// E a função pode estar escrita na forma antiga ou anônima ou em forma de arrow function
 
 // Crie uma função chamada calcular que receberá como parâmetro: dois números e uma função de callback
+
+/*
 function calcular (n1, n2, callback){
   return callback(n1, n2);
 }
+*/
+
+const calcular = (n1, n2, callback) => callback(n1, n2); // arrow function
+
 
 
 // Calcular em arrow function ficaria assim:
-const calcular2 =  (n1, n2, callback) => callback(n1, n2);
+//const calcular2 =  (n1, n2, callback) => callback(n1, n2);
 
 
 
@@ -136,7 +182,7 @@ console.log(`A ordem crescente de 100 e 20 é ${resultadoOrdenacao}`);
 
 
 
-// Volte nas funções e refatore-as na sintaxe ES6
+// Volte nas funções e refatore-as na sintaxe ES6 OK
 
 console.log('-----------------------------------------------------')
 // ----------------------------------------------
@@ -159,10 +205,22 @@ const resultatoAprovacao0 = (if (media >= 7) {
   return `Reprovado`
 })*/
 
-const resultadoAprovacao = (media >= 7) ? "Aprovada" : "Reprovada";
+const resultadoAprovacao = (media >= 7) ? "Aprovada" : "Reprovada";  // operador ternário
 // condição ? true : false
 
 console.log(`A aluna foi ${resultadoAprovacao}`)
+
+
+// Ternário operator: É uma forma diferente de declarar if else simples (com poucas linhas)
+// Funciona assim: (condição) ? retorno caso verdadeiro : retorno caso falso
+// A condição vai entre parênteses, os retornos não necessariamente
+
+
+
+
+
+
+
 
 console.log('-----------------------------------------------------')
 // ----------------------------------------------
@@ -196,6 +254,11 @@ console.log(`Meu pokemon é o ${name} e é do tipo ${type} e tem altura de ${hei
 
 
 
+// Desestruturação de objetos: É uma forma diferente de capturar os valores das chaves dos objetos
+
+// Ao declararmos constantes com os nomes idênticos ao nomes das propriedades e declararmo-nas
+// entre {}, o JS entende que são variáveis que recebem as propriedades de um objeto
+
 
 
 
@@ -208,33 +271,19 @@ console.log('Datas 🗓')
 // Outro exemplo de um objeto JavaScript é o Date.
 
 // Vamos criar a variável chamada hoje que irá receber a data de hoje.
-/*
+
 const hoje = new Date();
-console.log(hoje);
-const dia = hoje.getDate;
-console.log(dia);
+console.log(hoje); // 2020-09-06T00:54:12.729Z Mostra a hora atual no meridiano de Greenwich
+const dia = hoje.getDate();
+const mes = hoje.getMonth();
+const ano = hoje.getFullYear();
+console.log(dia, mes, ano); // 20 08 2020 Ele conta o m~es a partir do zero. Não sei por quê
 
-const minhaDataNascimento = new Date(1997, 3, 24);
+const dataFormatada = hoje.toLocaleDateString("pt-BR"); // 20/09/2020 ou 2020-9-5 depende do pc
+const dataFormatada2 = hoje.toLocaleString("pt-BR"); // 20/09/2020 ou 2020-9-5 21:59:28
+console.log(dataFormatada);
+console.log(dataFormatada2);
 
-//const hoje = new Date()
-
-//console.log(hoje) // 2020-09-05T10:56:49.693Z
-
-//const data = hoje.getDate()
-const mes = hoje.getMonth()
-const ano = hoje.getFullYear()
-
-console.log(data, mes, ano) // 5 8 2020
-
-const dataFormatada = hoje.toLocaleDateString('pt-BR')
-console.log(dataFormatada) // 05/09/2020
-
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-const dataLonga = hoje.toLocaleDateString('pt-BR', options)
-console.log(dataLonga) // sábado, 5 de setembro de 2020
-
-
-*/
 
 
 
@@ -250,6 +299,13 @@ console.log(dataLonga) // sábado, 5 de setembro de 2020
 
 // Vamos criar uma data específica. Lembrando que mês de Janeiro é 0 no Javascript 🤷🏻‍♀️
 
+const minhaDataNascimento = new Date(1997, 3, 24); // isso, abril é 3 pra isso
+console.log(minhaDataNascimento); //1997-04-24T03:00:00.000Z
+
+// Options você passa e edita o formato da data
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+const dataLonga = minhaDataNascimento.toLocaleDateString('pt-BR', options)
+console.log(dataLonga) // 1997 M04 24, Thu Por algum motivo não está capturando em português
 
 
 
@@ -352,6 +408,7 @@ const pessoa3 = {
   }
 }
 
+
 // Esse aqui tá certo, só não tá resumido
 const pessoa4 = {
   nome: 'Yasminn',
@@ -403,11 +460,17 @@ const pessoa5 = {
 }
 
 
-
+// Sobre objetos e o this: o this se refere a uma propriedade do objeto quando esta é referenciada dentro dele!
+// Importante: O this não pega dentro de arrow functions!
+// Objetos são legais, mas quando temos muuuitos objetos com as propriedades e temos que 
+// escrever todos, faz mais sentido criar uma classe com um construtor. 
+// Classe é um objeto que pode ser instanciado =)
 
 // Vamos utilizar o formato ES6 para refatorar a constante pessoa.
 
 // Quero criar as variáveis pessoa2 e pessoa3 com as mesmas propriedades, mas alterando os valores de nome, sobrenome, altura e peso
+
+
 
 
 
@@ -455,6 +518,14 @@ class Pessoa {
 
 
 
+// O this também não pega em arrow functions também
+// As funções não precisam ser declaradas com os dois pontos de propriedade, mas continuam
+// sendo propriedades da classe
+// Não usar arrow function nas classes
+
+
+
+
 
 
 console.log('-----------------------------------------------------')
@@ -477,46 +548,87 @@ console.log(lista[2]);
 
 
 // Informe o tamanho de cada array
+console.log(lista.length);
 
-
-
+const primeiro = lista[0];
+console.log(primeiro);
 
 
 
 
 // Faça a desestruturação do array
 
+const [primeiroItem, segundoItem, terceiroItem] = lista;
 
-
-
+console.log(primeiroItem, segundoItem, terceiroItem);
 
 
 
 // Possuo 4 tias. Os dados delas estão armazenados no array de objetos dentro do arquivo db.js
 // Vamos importar esses dados para podermos usá-los durante nosso exercício de revisão.
 const db = require('./db');
+
 console.log(db);
 
+// No arquivo db há um objeto que tem uma única propriedade, e esta propriedade se chama tias
+// E o seu valor é uma a lista de objetos tias
 
-const { tias } = db;
+
+const { tias } = db; // Então aqui pegamos a propriedade tias do objeto maior (o  module.exports)
+
 console.log(tias);
+
 console.table(tias);
+
 console.table(lista);
+
+
+
+// O console.table plota arrays (de qualquer coisa) em tabelas, muito legal.
 
 
 
 console.log('-----------------------------------------------------')
 // ----------------------------------------------
-// MÉTODOS DE ITERAÇÃO
+// MÉTODOS DE ITERAÇÃO (EM ARRAYS)
 console.log('Métodos iteração ')
+
+
+// Iterações nos arrays
+let i = 0
+
+while (i < numbers.length) {
+  const dobro = numbers[i] * 2
+  console.log(dobro)
+  i++
+}
+
+// 18 4 10
+
+
+// for... of: muito legal! Basicamente: para cada item do meu array, faça isso usando ele
+// O array não é alterado. A não ser que isso seja feito voluntariamente, acredito.
+for (number of numbers) {
+  const dobro = number * 2;
+
+  console.log(dobro)
+}
+ // 18 4 10
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Mostre a tabela das tias pelo console.table()
 
-
-
-const tiasSP = tias.filter(item => item.local === "SP");
-
-console.table(tiasSP);
 
 
 
@@ -525,11 +637,39 @@ console.table(tiasSP);
 console.log('-----------------------------------------------------')
 console.log('filter()')
 // filter
-// Filtre as tias que moram em SP e mostre no console.
+// Filtre as tias que moram em SP e mostre no console. Foi feito logo acima
 
 
 
 
+const tiasSP = tias.filter(item => item.local === "SP");
+
+console.table(tiasSP);
+
+/*  
+Agora sabemos que o que vai dentro do filter é uma função callback, e além disso, neste caso, a função está escrita da forma de arrow function!
+
+função antiga:
+function nome (item){
+  return item.local === "SP";
+} 
+
+arrow function:
+const nome = item => item.local === "SP";
+
+O método filter para cada true recebido da callback, pega o elemento e bota numa nova lista e retorna no final a nova lista com todos os elementos que fizeram a callback retorna true. Ou seja, ele é um filtro.
+
+
+*/
+
+
+
+
+
+
+const numerosImpares = numbers.filter(item => item % 2 !== 0)
+
+console.log(numerosImpares) // [9, 5] OK
 
 
 
@@ -538,6 +678,8 @@ console.log('map()')
 // map
 // Crie um novo array chamado tiasMaisChegadas e adicione uma propriedade chamada cuidouDeMim que recebe um valor booleano. Caso a tia teve até 2 filhos, isso significa que ela cuidou de mim e seu valor é true. Caso ela teve mais que 2 filhos, o valor da propriedade cuidouDeMim é false.
 
+/*
+Função na forma antiga
 
 function cuidar(tia){
   if (tia.filhos <= 2){
@@ -547,9 +689,22 @@ function cuidar(tia){
   }
 }
 
-const tiasCuidou = tias.map(cuidar);
+
+*/
+
+// cuidar em arrow function 
+const cuidar = tia => (tia.filhos <= 2) ? tia.cuidouDeMim = true : tia.cuidouDeMim = false;
+
+
+
+const tiasCuidou = tias.map(cuidar); 
 console.table(tias);
 console.log(tiasCuidou);
+
+
+// Importante demais: O método map modifica diretamente o nosso array, adicionando o que quisermos!
+
+// Lembrando, o parâmetro que a callback recebe, é o elemento do array SEMPRE
 
 
 
@@ -576,16 +731,28 @@ const comparar = (a, b) => {
 // 2 e 5 -> retorna valor negativo -> 2 - 5 = - 3 negativo
 // 2 e 2 -> retorna valor zero -> 2 - 2 = 0
 // Então se eu retornar a - b, já dá certo
+// Essa demonstração foi para provar que a subtração já retorna uma forma de ordenação
+// a sendo o primeiro e b o segundo elemento, a - b retorna a ordem crescente, 
+// enquanto b - a retorna decrescente!
 
+
+
+/*
 const comparar2 = (a, b) => {
   return a - b;
 }
+
+const comparar3 = (a, b) => a - b;
+
+*/
 
 //numbers.sort(comparar2); // o sort modifica o array, não retorna outro!!
 
 numbers.sort((a, b) => a - b);
 console.log(numbers); // usei o comparar e alterei para comparar2 e usei de novo, depois mudei para ES6 e
 // usei de novo kkk
+
+
 
 // Refatore a função comparar e ordene numbers em ordem crescente
 
@@ -603,12 +770,17 @@ function ordenarTiasPorIdade(tia1, tia2){
 }*/
 
 tias.sort((tia1, tia2) => tia2.idade - tia1.idade);
-console.table(tias);
 
+console.table(tias); // OK
 
+/*
+Uma nota sobre o find: 
 
+O método find executa a função callback uma vez para cada elemento presente no array até que encontre um onde callback retorne o valor true. Se o elemento é encontrado, find retorna imediatamente o valor deste elemento. Caso contrário, find retorna undefined. 
 
+Útil para quando queremos apenas um elemento que satisfaça uma condição 
 
+*/
 
 
 console.log('-----------------------------------------------------')
@@ -644,6 +816,13 @@ console.log(qtdeNetos);*/
 
 const qtdeNetos = tias.reduce((acumulador, item) => acumulador + item.filhos, 1);
 console.log(qtdeNetos);
+
+/*
+Sobre Spread operator
+Podemos "copiar" todas as propriedades existentes dentro de um objeto e repassá-los para num novo objeto
+*/
+
+
 
 // ----------------------------------------------
 console.log('-----------------------------------------------------')
--- projeto/index.js
+++ projeto/index.js
@@ -1,5 +1,24 @@
-console.log('--------------------------------------')
-console.log('     Projeto Carrinho de Compras     ')
-console.log('--------------------------------------')
+console.log('--------------------------------------');
+console.log('     Projeto Carrinho de Compras     ');
+console.log('--------------------------------------');
 
-const db = require('./database')
+const db = require('./database');
+const query = require('synchronous-user-input');
+
+const { produtos } = db;
+produtos.sort((product1, product2) => product1.preco - product2.preco);
+//console.table(produtos);
+/*
+let stillBuying = 's';
+while (stillBuying.toLowerCase() === 's') {
+    const id = query('Digite o id do produto desejado: ');
+    const quantity = query('Digite a quantidade que gostaria de adquirir: ');
+    stillBuying = query('Deseja continuar comprando? S ou N: ');
+}
+*/
+const shoppingCart = [];
+/* 
+const hasDiscountCoupon = query('Você possui cupom de desconto? S ou N: ');
+const discountCoupon = query('De quanto é o seu cupom? ');
+*/
+
