21d0f87f82d1bbc7e3ae16626168e2916459b2d9
Luana-Leticia <luanaleticiasouza@hotmail.com>
2020-09-09 13:16:47 -0300

    Adiciona sistema que pergunta se é cliente ou funcionário e conserta função de validar desconto

--- projeto/index.js
+++ projeto/index.js
@@ -6,9 +6,7 @@ const db = require('./database');
 const query = require('synchronous-user-input');
 
 const { produtos } = db;
-
 produtos.sort((product1, product2) => product1.preco - product2.preco);
-
 const categories = produtos.reduce((acumulator, product) => (acumulator.includes(product.categoria)) ? acumulator : [...acumulator, product.categoria], []);
 
 const getProductById = (id, productsList) => {
@@ -20,106 +18,7 @@ const validateQuantity = quantity => (quantity > 0) ? quantity : 'Você digitou 
 
 const validateCategoryNumber = categoryNumber => (categoryNumber >= 0 && categoryNumber < categories.length) ? categoryNumber : 'Você digitou um número de categoria inválido. Tente novamente!';
 
-const shoppingCart = [];
-
-let buyInAnotherCategory = 's';
-let categoryNumber = -1;
-
-let stillBuyingInCategory = 's';
-let addedProduct;
-
-while (buyInAnotherCategory.toLowerCase() === 's') {
-    console.table(categories);
-    categoryNumber = parseInt(query(`Escolha uma das categorias acima (número de 0 a ${categories.length - 1}): `));
-    categoryNumber = validateCategoryNumber(categoryNumber);
-    while(typeof categoryNumber === 'string'){
-        console.log(categoryNumber);
-        categoryNumber = parseInt(query(`Escolha uma das categorias acima (número de 0 a ${categories.length - 1}): `));
-        categoryNumber = validateCategoryNumber(categoryNumber);
-    }
-    const productsOfSpecificCategory = produtos.filter(product => product.categoria === categories[categoryNumber]);
-    console.table(productsOfSpecificCategory);
-
-    stillBuyingInCategory = 's';
-
-    while (stillBuyingInCategory.toLowerCase() === 's') {
-        let id = parseInt(query('Digite o id do produto desejado: '));
-        let product = getProductById(id, productsOfSpecificCategory);
-        while (typeof product === 'string') {
-            console.log(product);
-            id = parseInt(query('Digite o id do produto desejado: '));
-            product = getProductById(id, productsOfSpecificCategory);
-        }
-        let quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
-        quantity = validateQuantity(quantity);
-        while (typeof quantity === 'string') {
-            console.log(quantity);
-            quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
-            quantity = validateQuantity(quantity);
-        }
-
-        addedProduct = { ...product, quantidade: quantity }
-        shoppingCart.push(addedProduct);
-
-        stillBuyingInCategory = query('Deseja continuar comprando nesta categoria? S ou N: ');
-    }
-
-    buyInAnotherCategory = query('Você deseja comprar em outra categoria? S ou N: ');
-}
-
-shoppingCart.sort((product1, product2) => product1.preco - product2.preco);
-
-discountCouponsList = [
-    {
-        promoCode: 'loja10',
-        dueDate: new Date(2020, 12, 31),
-        value: 0.10,
-        category: categories
-    },
-    {
-        promoCode: 'loja15',
-        dueDate: new Date(2020, 8, 30),
-        value: 0.15,
-        validityCategories: categories
-    },
-    {
-        promoCode: 'loja30',
-        dueDate: new Date(2020, 01, 31),
-        value: 0.3,
-        validityCategories: categories
-    },
-    {
-        promoCode: 'bfinformatica',
-        dueDate: new Date(2020, 11, 31),
-        value: 0.3,
-        validityCategories: ['informática']
-    }
-];
-
-const validateCouponByPromoCode = promoCode => {
-    const coupon = discountCouponsList.find(coupon => coupon.promoCode === promoCode && coupon.dueDate > new Date());
-    return (typeof coupon === 'object') ? coupon : 'Cupom inválido ou expirado!';
-}
-
-const hasDiscountCoupon = query('Você possui cupom de desconto? S ou N: ');
-let coupon;
-
-if (hasDiscountCoupon.toLowerCase() === 's') {
-    const promoCode = query('Digite o código do seu cupom: ');
-    coupon = validateCouponByPromoCode(promoCode);
-    if(typeof coupon === 'string'){
-        console.log(coupon);
-        coupon = {
-            promoCode: 'none',
-            dueDate: new Date(2000, 0, 1),
-            value: 0,
-            validityCategories: categories
-        }
-    }else{
-        console.log('Esse cupom é válido nas seguintes categorias:');
-        console.table(coupon.validityCategories);
-    }
-}
+const validateUserNumber = userNumber => (userNumber === 1 || userNumber === 2) ? userNumber : 'Você digitou um número de uma opção inválida de usuário. Tente novamente!';
 
 class Pedido {
     constructor(productsList, coupon, orderDate = new Date()){
@@ -158,16 +57,144 @@ class Pedido {
     }
 }
 
-const pedido1 = new Pedido(shoppingCart, coupon);
-pedido1.calculateTotalItems();
-pedido1.calculateSubtotalPrice();
-pedido1.calculateDiscount();
-pedido1.calculateTotalPrice();
+discountCouponsList = [
+    {
+        promoCode: 'loja10',
+        dueDate: new Date(2020, 12, 31),
+        value: 0.10,
+        validityCategories: categories
+    },
+    {
+        promoCode: 'loja15',
+        dueDate: new Date(2020, 8, 30),
+        value: 0.15,
+        validityCategories: categories
+    },
+    {
+        promoCode: 'loja30',
+        dueDate: new Date(2020, 01, 31),
+        value: 0.3,
+        validityCategories: categories
+    },
+    {
+        promoCode: 'bfinformatica',
+        dueDate: new Date(2020, 11, 31),
+        value: 0.3,
+        validityCategories: ['informática']
+    }
+];
 
-console.log('--------------------------------------');
-console.log('     Seu Carrinho de Compras     ');
-console.log('--------------------------------------');
-console.table(shoppingCart);
+const validateCouponByPromoCode = promoCode => {
+    const coupon = discountCouponsList.find(coupon => coupon.promoCode === promoCode && coupon.dueDate > new Date());
+    const emptyCoupon = {
+        promoCode: 'none',
+        dueDate: new Date(2000, 0, 1),
+        value: 0,
+        validityCategories: categories
+    }
+    return (typeof coupon === 'object') ? coupon : emptyCoupon;
+}
 
-const { subtotalPrice, discountValue, totalPrice, orderDate } = pedido1;
-console.log(`Valor subtotal: R$ ${subtotalPrice} \nValor do desconto: R$ ${discountValue} \nValor total: R$ ${totalPrice} \nData da compra: ${orderDate}`);
+let userNumber = 0;
+let wantCloseSystem = 'n';
+
+let requestsList = [];
+let shoppingCart;
+let request;
+
+while (wantCloseSystem === 'n') {
+    userNumber = parseInt(query('Você é cliente ou funcionário? (1 - Cliente ou 2 - Funcionário) '));
+    userNumber = validateUserNumber(userNumber);
+    while (typeof userNumber === 'string') {
+        console.log(userNumber);
+        userNumber = parseInt(query('Você é cliente ou funcionário? (1 - Cliente ou 2 - Funcionário) '));
+        userNumber = validateUserNumber(userNumber);
+    }
+
+    if (userNumber === 1) {
+        shoppingCart = [];
+
+        let buyInAnotherCategory = 's';
+        let categoryNumber = -1;
+
+        let stillBuyingInCategory = 's';
+        let addedProduct;
+
+        while (buyInAnotherCategory.toLowerCase() === 's') {
+            console.table(categories);
+            categoryNumber = parseInt(query(`Escolha uma das categorias acima (número de 0 a ${categories.length - 1}): `));
+            categoryNumber = validateCategoryNumber(categoryNumber);
+            while (typeof categoryNumber === 'string') {
+                console.log(categoryNumber);
+                categoryNumber = parseInt(query(`Escolha uma das categorias acima (número de 0 a ${categories.length - 1}): `));
+                categoryNumber = validateCategoryNumber(categoryNumber);
+            }
+            const productsOfSpecificCategory = produtos.filter(product => product.categoria === categories[categoryNumber]);
+            console.table(productsOfSpecificCategory);
+
+            stillBuyingInCategory = 's';
+
+            while (stillBuyingInCategory.toLowerCase() === 's') {
+                let id = parseInt(query('Digite o id do produto desejado: '));
+                let product = getProductById(id, productsOfSpecificCategory);
+                while (typeof product === 'string') {
+                    console.log(product);
+                    id = parseInt(query('Digite o id do produto desejado: '));
+                    product = getProductById(id, productsOfSpecificCategory);
+                }
+                let quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
+                quantity = validateQuantity(quantity);
+                while (typeof quantity === 'string') {
+                    console.log(quantity);
+                    quantity = parseInt(query('Digite a quantidade que gostaria de adquirir: '));
+                    quantity = validateQuantity(quantity);
+                }
+
+                addedProduct = { ...product, quantidade: quantity }
+                shoppingCart.push(addedProduct);
+
+                stillBuyingInCategory = query('Deseja continuar comprando nesta categoria? S ou N: ');
+            }
+
+            buyInAnotherCategory = query('Você deseja comprar em outra categoria? S ou N: ');
+        }
+
+        shoppingCart.sort((product1, product2) => product1.preco - product2.preco);
+
+        const hasDiscountCoupon = query('Você possui cupom de desconto? S ou N: ');
+        let coupon;
+
+        if (hasDiscountCoupon.toLowerCase() === 's') {
+            const promoCode = query('Digite o código do seu cupom: ');
+            coupon = validateCouponByPromoCode(promoCode);
+            if (typeof coupon.promoCode === 'none') {
+                console.log('Cupom inválido ou expirado!');
+            } else {
+                console.log('Esse cupom é válido nas seguintes categorias:');
+                console.table(coupon.validityCategories);
+            }
+        } else {
+            coupon = validateCouponByPromoCode('none');
+        }
+
+        request = new Pedido(shoppingCart, coupon);
+        request.calculateTotalItems();
+        request.calculateSubtotalPrice();
+        request.calculateDiscount();
+        request.calculateTotalPrice();
+
+        console.log('--------------------------------------');
+        console.log('     Seu Carrinho de Compras     ');
+        console.log('--------------------------------------');
+        console.table(shoppingCart);
+
+        const { subtotalPrice, discountValue, totalPrice, orderDate } = request;
+        console.log(`Valor subtotal: R$ ${subtotalPrice} \nValor do desconto: R$ ${discountValue} \nValor total: R$ ${totalPrice} \nData da compra: ${orderDate}`);
+
+        requestsList.push(request);
+    } else {
+
+    }
+
+    wantCloseSystem = query('Deseja encerrar o sistema? S ou N: ');
+}
